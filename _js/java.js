762e9315bb348ba83fae352e7293b5fbb6fcde38
GideaoDSantos <gideao.d.santos.aluno.senai.br>
2025-09-02 14:58:46 -0300

    funções feitas

--- _js/java.js
+++ _js/java.js
@@ -8,33 +8,33 @@ function playPause() {
 }
 
 function makeBig() {
-
+    meuVideo.width = 700;
 }
 
 function makeSmall() {
-
+    meuVideo.width = 300;
 }
 
 function makeNormal() {
-
+    meuVideo.width = 450;
 }
 
 function telaCheia() {
-
+    meuVideo.requestFullscreen();
 }
 
 function aumentarVolume() {
-
+    meuVideo.volume += 0.1;
 }
 
 function diminuirVolume() {
-
+    meuVideo.volume -= 0.1;
 }
 
 function mutar() {
-
+    meuVideo.muted = true;
 }
 
 function desmutar() {
-
+    meuVideo.muted = false;
 }
