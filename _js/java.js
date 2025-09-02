796fc90dd59235dd0070dbc8e1e277fc53566c97
GideaoDSantos <gideao.d.santos.aluno.senai.br>
2025-09-02 14:42:01 -0300

    adicionando botões e funções

---
+++ _js/java.js
@@ -0,0 +1,40 @@
+var meuVideo = document.getElementById("player");
+
+function playPause() {
+    if (meuVideo.paused)
+        meuVideo.play();
+    else
+        meuVideo.pause();
+}
+
+function makeBig() {
+
+}
+
+function makeSmall() {
+
+}
+
+function makeNormal() {
+
+}
+
+function telaCheia() {
+
+}
+
+function aumentarVolume() {
+
+}
+
+function diminuirVolume() {
+
+}
+
+function mutar() {
+
+}
+
+function desmutar() {
+
+}
